ca74222d728918ef4d548d7cf51c0883c7e618cf
Ismael VAS <ismael.vas@Digital-Grenoble.local>
2024-02-16 09:57:45 +0100

    first commit

---
+++ src/test/java/com/squareGames/squareGamesAPI/SquareGamesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.squareGames.squareGamesAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SquareGamesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
